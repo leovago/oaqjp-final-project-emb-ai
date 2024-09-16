8bdc99843195f09f41406813276e67264327d740
leovago <leo.vargas.glez@gmail.com>
2024-09-15 23:11:17 -0400

    Adding server.py

---
+++ server.py
@@ -0,0 +1,48 @@
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+#Initiate the flask app :
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows multiple emotions, 
+        their confidence scores and the dominant emotion 
+        for the provided text.
+    '''
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    anger_score = response['anger']
+    disgust_score = response['disgust']
+    fear_score = response['fear']
+    joy_score = response['joy']
+    sadness_score = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    return_text = "For the given statement, the system response is "
+    return_text += "'anger': {}, ".format(anger_score)
+    return_text += "'disgust' {}, ".format(disgust_score)
+    return_text += "'fear' {}, ".format(fear_score)
+    return_text += "'joy' {}, ".format(joy_score)
+    return_text += "'sadness' {}. ".format(sadness_score)
+    return_text += "The dominant emotion is <b>{}</b>.".format(dominant_emotion)
+
+    return return_text
+    
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    ''' This functions executes the flask app and deploys it on localhost:5000
+    '''
+    app.run(host="0.0.0.0", port=5000)
